ccef99a67dbf1dff0418bda15518142b2bdf5170
Simon Hudon <simon.hudon@gmail.com>
2017-05-22 15:49:58 -0400

    Added the type class functor_pair to allow the use of functors (soon applicative) in traversable on Type u -> Type v (instead of Type u -> Type u)

--- data/functor.lean
+++ data/functor.lean
@@ -1,5 +1,16 @@
 
-universe variables u v u'
+universe variables u v w u' v' w'
+
+class functor_pair (f : Type u → Type v) (g : Type u' → Type v') :=
+ (f_functor : functor f)
+ (g_functor : functor g)
+ (map : ∀ {α β}, (α → β) → f α → g β)
+ (map_fmap_comm : ∀ {α β β' γ}
+            (h : β → γ)    (hp : α → β)
+            (hp' : β' → γ) (h' : α → β')
+            (x : f α),
+    h ∘ hp = hp' ∘ h' →
+    functor.map h (map hp x) = map hp' (functor.map h' x))
 
 section thms
 
@@ -41,10 +52,22 @@ lemma map_comp (f : α → β) (g : β → γ)
 
 end identity
 
-instance : functor identity :=
+instance identity_functor : functor identity :=
 { map := @identity.map
 , id_map := @identity.id_map
 , map_comp := @identity.map_comp }
+
+instance : functor_pair identity identity :=
+{ f_functor := identity_functor
+, g_functor := identity_functor
+, map := @identity.map
+, map_fmap_comm :=
+  begin
+    intros α β β' γ,
+    intros h hp hp' h' x,
+    intros H, unfold functor.map,
+    rw [-identity.map_comp,-identity.map_comp,H],
+  end }
 
 lemma identity.fmap_mk {α β : Type v}  (f : α → β) (x : α)
 : f <$> identity.mk x = identity.mk (f x) := rfl
@@ -56,9 +79,9 @@ structure compose (f : Type u → Type u') (g : Type v → Type u) (α : Type v)
 
 namespace compose
 
-variables {f : Type u → Type u'} {g : Type v → Type u}
-
 section functor
+
+variables {f : Type u → Type u'} {g : Type v → Type u}
 
 variables [functor f] [functor g]
 variables {α β γ : Type v}
@@ -78,6 +101,18 @@ lemma map_comp (g_1 : α → β) (h : β → γ) : ∀ (x : compose f g α),
 by { unfold map, rw [functor.map_comp' g_1 h,functor.map_comp], }
 
 end functor
+section functor_pair
+
+variables {f  : Type v  → Type w}  {g :  Type u  → Type v}
+variables {f' : Type v' → Type w'} {g' : Type u' → Type v'}
+
+variables [functor_pair f f'] [functor_pair g g']
+variables {α : Type u} {β : Type u'}
+
+def map_pair (h : α → β) : compose f g α → compose f' g' β
+  | ⟨ x ⟩ := ⟨ functor_pair.map f' (functor_pair.map g' h) x ⟩
+
+end functor_pair
 end compose
 
 instance functor_compose {f : Type u → Type u'} {g : Type v → Type u}
@@ -86,6 +121,32 @@ instance functor_compose {f : Type u → Type u'} {g : Type v → Type u}
 { map := @compose.map f g _ _
 , id_map := @compose.id_map f g _ _
 , map_comp := @compose.map_comp f g _ _ }
+
+instance compose_functor_pair
+  {f :  Type v  → Type w}  {g  : Type u  → Type v}
+  {f' : Type v' → Type w'} {g' : Type u' → Type v'}
+  [functor_pair f f'] [functor_pair g g']
+: functor_pair (compose f g) (compose f' g') :=
+{ f_functor := @functor_compose f g
+       (functor_pair.f_functor f f')
+       (functor_pair.f_functor g g')
+, g_functor := @functor_compose f' g'
+       (functor_pair.g_functor f f')
+       (functor_pair.g_functor g g')
+, map := @compose.map_pair f g _ _ _ _
+, map_fmap_comm :=
+  begin
+    intros α β β' γ,
+    intros h hp hp' h' x H,
+    unfold functor.map,
+    cases x with x,
+    unfold compose.map_pair compose.map,
+    apply congr_arg,
+    rw [functor_pair.map_fmap_comm],
+    apply funext, intro i, unfold comp fmap,
+    rw [functor_pair.map_fmap_comm],
+    apply H,
+  end }
 
 lemma compose.fmap_mk {α β : Type u'}
   {f : Type u → Type v} {g : Type u' → Type u}

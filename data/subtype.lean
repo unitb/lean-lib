3b73f89e10cf3912b0542476b5c23ef1d53e4d60
Simon Hudon <simon.hudon@gmail.com>
2017-07-23 13:15:01 -0400

    sketched a proof of liveness in the proof of soundness of the verification rules

--- data/subtype.lean
+++ data/subtype.lean
@@ -8,3 +8,9 @@ lemma coe_subtype_eq_self {x : α} {P : α → Prop}
   (h : P x)
 : ↑ (⟨x, h⟩ : subtype P) = x :=
 rfl
+
+@[simp]
+lemma coe_eq_subtype_val {P : α → Prop}
+  (x : subtype P)
+: ↑ x = x.val :=
+rfl

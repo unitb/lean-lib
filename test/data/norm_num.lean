8842609552fbcb24d1501a11980eef0eeb80dc06
Simon Hudon <simon.hudon@gmail.com>
2017-11-24 23:59:52 -0500

    norm_num was moved to mathlib

--- test/data/norm_num.lean
+++ test/data/norm_num.lean
@@ -1,24 +1,30 @@
 
-import util.data.norm_num
+import tactic.norm_num
 
 example : 374 + (32 - (2 * 8123) : ℤ) - 61 * 50 = 86 + 32 * 32 - 4 * 5000
       ∧ 43 ≤ 74 + (33 : ℤ) :=
-by { norm_sub_expr, split, refl, trivial }
+by { norm_num }
 
 example : (1103 : ℤ) ≤ (2102 : ℤ) :=
-by { norm_sub_expr, trivial }
+by { norm_num }
 
 example : (11047462383473829263 : ℤ) ≤ (21048574677772382462 : ℤ) :=
-by { norm_sub_expr, trivial,  }
+by { norm_num,  }
 
 example : (210485742382937847263 : ℤ) ≤ (1104857462382937847262 : ℤ) :=
-by { norm_sub_expr, trivial }
+by { norm_num }
 
 example : (210485987642382937847263 : ℕ) ≤ (11048512347462382937847262 : ℕ) :=
-by { norm_sub_expr, trivial }
+by { norm_num }
 
 example : (210485987642382937847263 : ℚ) ≤ (11048512347462382937847262 : ℚ) :=
-by { norm_sub_expr, trivial }
+by { norm_num }
+
+example : (210485987642382937847263 : ℚ) < (11048512347462382937847262 : ℚ) :=
+by { norm_num }
+
+example : (210485987642382937847263 : ℚ) ≠ (11048512347462382937847262 : ℚ) :=
+by { norm_num }
 
 local infix ^ := pow_nat
 

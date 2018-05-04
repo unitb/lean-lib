8881f3317b074de5e8d70de098f4ce96da63f1c0
Simon Hudon <simon.hudon@gmail.com>
2018-05-03 22:23:49 -0400

    sum goodies

--- src/util/data/sum.lean
+++ src/util/data/sum.lean
@@ -1,9 +1,10 @@
 
-universe variables u v
+universe variables u v u' v'
 
 namespace sum
 
-variables {α : Type u} {β : Type v}
+variables {α : Type u}   {β : Type v}
+          {α' : Type u'} {β' : Type v'}
 
 def is_left : α ⊕ β → bool
   | (sum.inl _) := tt
@@ -15,5 +16,13 @@ def is_right : α ⊕ β → bool
 
 instance {α : Type u} : functor (sum α) :=
  { map := λ β γ f x, sum.rec_on x sum.inl (sum.inr ∘ f) }
+
+def bimap (f : α → α') (g : β → β') : α ⊕ β → α' ⊕ β'
+ | (inl x) := inl $ f x
+ | (inr x) := inr $ g x
+
+def swap : α ⊕ β → β ⊕ α
+ | (inl x) := inr x
+ | (inr x) := inl x
 
 end sum
